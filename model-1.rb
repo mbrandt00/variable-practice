afb07237eced1ddf1a992aeae9ef50f1ad322e84
Michael Brandt <msbrandt00@gmail.com>
2021-11-04 20:28:17 -0600

    Add arrays

--- model-1.rb
+++ model-1.rb
@@ -14,4 +14,9 @@ hungry = true
 
 bored = false
 
-puts (tired && hungry && bored) #false boolean eval 
+puts (tired && hungry && bored) #false boolean eval
+
+current_pieces = ['Chopin Op. 10 No. 3', 'Chopin Op. 10 8', 'Prelude and Fugue Bach']
+
+
+days_of_week = %w(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
